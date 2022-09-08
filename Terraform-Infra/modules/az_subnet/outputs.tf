fc350b16daab85632e66b60b1a614ba776ba9529
Viktoria-dev <66776179+Viktoria-dev@users.noreply.github.com>
2022-09-08 09:58:38 +0300

    Update outputs.tf

--- Terraform-Infra/modules/az_subnet/outputs.tf
+++ Terraform-Infra/modules/az_subnet/outputs.tf
@@ -1,11 +1,3 @@
-# output "resource_group_name" {
-#   value = azurerm_resource_group.dev.name
-# }
-
-# output "location" {
-#   value = azurerm_resource_group.dev.location
-# }
-
 output "azurerm_network_interface" {
   value = azurerm_network_interface.dev.id
 }
