5cfd02fe352dd9bf80994ec55d2cd62ab1e75cf9
Viktoria-dev <66776179+Viktoria-dev@users.noreply.github.com>
2022-09-08 09:58:01 +0300

    Update main.tf

--- Terraform-Infra/modules/az_network/main.tf
+++ Terraform-Infra/modules/az_network/main.tf
@@ -5,12 +5,6 @@ resource "azurerm_virtual_network" "dev" {
   resource_group_name = var.resource_group
 }
 
-# resource "azurerm_subnet" "dev" {
-#   name                 = var.vnet
-#   resource_group_name  = var.resource_group
-#   virtual_network_name = var.resource_group
-#   address_prefixes     = ["10.0.5.0/24"]
-# }
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "my_terraform_nsg" {
@@ -30,16 +24,4 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     destination_address_prefix = "*"
   }
 }
-
-# resource "azurerm_network_interface" "dev" {
-#   name                = "dev-nic"
-#   location            = var.location
-#   resource_group_name = var.resource_group
-
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.dev.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-# }
 
